dad09d17f69729032ee5054f2f8264eea45330a3
Muhammad Naufal Fadhil Rahmat <naufalfadhil342@gmail.com>
2023-09-04 13:50:30 +0700

    updated index.js v1

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: '*',
+    origin: ['https://workouts-eight.vercel.app', 'http://localhost:3000', 'https://github.com/'],
     credentials: true,
   })
 );
